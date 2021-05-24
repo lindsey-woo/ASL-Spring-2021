fc7aa850d69176301e0afcfd147b7b8a0fb0c12b
ASL-Laptop <lindseyhwoo@gmail.com>
2021-05-24 14:48:58 -0400

    add jackal_slam

---
+++ jackal_slam/build/CTestTestfile.cmake
@@ -0,0 +1,16 @@
+# CMake generated Testfile for 
+# Source directory: /home/administrator/workspaces/lindsey/src
+# Build directory: /home/administrator/workspaces/lindsey/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("scan_tools/scan_tools")
+subdirs("jackal_cartographer_navigation")
+subdirs("scan_tools/laser_scan_sparsifier")
+subdirs("scan_tools/laser_scan_splitter")
+subdirs("scan_tools/ncd_parser")
+subdirs("scan_tools/laser_ortho_projector")
+subdirs("scan_tools/laser_scan_matcher")
+subdirs("scan_tools/polar_scan_matcher")
+subdirs("scan_tools/scan_to_cloud_converter")
---
+++ jackal_slam/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/administrator/workspaces/lindsey/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/administrator/workspaces/lindsey/devel;/opt/ros/melodic'.split(';') if '/home/administrator/workspaces/lindsey/devel;/opt/ros/melodic' != '' else []
